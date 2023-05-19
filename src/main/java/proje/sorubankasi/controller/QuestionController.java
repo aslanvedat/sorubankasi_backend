f766607952abccbbf9cebc10bc253d7f6488ff06
Onur TALANTIMUR <onurtalantimur@gmail.com>
2023-05-20 00:15:23 +0300

    controller has ben added

--- src/main/java/proje/sorubankasi/controller/QuestionController.java
+++ src/main/java/proje/sorubankasi/controller/QuestionController.java
@@ -1,9 +1,13 @@
 package proje.sorubankasi.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class QuestionController {
 
-
+    @GetMapping("/test")
+    public String test(){
+        return "Hello";
+    }
 }
